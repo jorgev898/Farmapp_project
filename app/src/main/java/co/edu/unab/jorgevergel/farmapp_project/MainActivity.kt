558b2d845598552b421c0f8fd22d1dff1474712a
jorgev898 <jorgeisaacvergelgarcia@gmail.com>
2024-10-23 08:53:06 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unab/jorgevergel/farmapp_project/MainActivity.kt
@@ -0,0 +1,47 @@
+package co.edu.unab.jorgevergel.farmapp_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.unab.jorgevergel.farmapp_project.ui.theme.Farmapp_projectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Farmapp_projectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Farmapp_projectTheme {
+        Greeting("Android")
+    }
+}
